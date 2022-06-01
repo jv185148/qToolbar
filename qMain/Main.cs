d20c718a6c91e160cda5ceaef7b77f2f414e3d79
Jaybee van der Linde <jv185148@ncr.com>
2022-06-01 12:51:43 +0200

    Adding first projects

---
+++ qMain/Main.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace qMain
+{
+    public class Main
+    {
+
+        public qCommon.Interfaces.iMain child;
+
+        public Main(qCommon.Interfaces.iMain child)
+        {
+            this.child = child;
+        }
+
+        public void Load()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
